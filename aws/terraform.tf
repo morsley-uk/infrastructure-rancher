74cf35b0983cb55e7c75e6a9aff06e4ef9791343
John Morsley <johnmorsley@gmail.com>
2020-03-18 13:40:33 +0000

    Add Rancher to a cluster.

---
+++ aws/terraform.tf
@@ -0,0 +1,27 @@
+﻿#     _______                   __
+#   |__   __|                 / _|
+#      | | ___ _ __ _ __ __ _| |_ ___  _ __ _ __ ___
+#      | |/ _ \ '__| '__/ _` |  _/ _ \| '__| '_ ` _ \
+#      | |  __/ |  | | | (_| | || (_) | |  | | | | | |
+#      |_|\___|_|  |_|  \__,_|_| \___/|_|  |_| |_| |_|
+#
+
+terraform {
+
+  required_version = "= 0.12.23" # https://github.com/hashicorp/terraform/releases
+
+  required_providers {
+    aws = {
+      version = "= 2.53.0" # https://registry.terraform.io/providers/hashicorp/aws/
+    }
+  }
+
+  backend "s3" {
+    bucket         = "morsley-io-terraform"
+    key            = "infrastructure"
+    region         = "eu-west-2"
+    dynamodb_table = "tfstatelock-rancher"
+    encypt         = true
+  }
+
+}
---
+++ aws/variables.tf
@@ -0,0 +1,44 @@
+﻿#   __      __        _       _     _           
+#   \ \    / /       (_)     | |   | |          
+#    \ \  / /_ _ _ __ _  __ _| |__ | | ___  ___ 
+#     \ \/ / _` | '__| |/ _` | '_ \| |/ _ \/ __|
+#      \  / (_| | |  | | (_| | |_) | |  __/\__ \
+#       \/ \__,_|_|  |_|\__,_|_.__/|_|\___||___/
+#                                             
+
+variable "region" {
+  default = "eu-west-2" # London
+}
+
+variable "access_key" {}
+
+variable "secret_key" {}
+
+variable "key_name" {
+  default = "morsley-io-rancher-k8s"
+}
+
+variable "keys_bucket" {
+  default = "morsley-io-rancher"
+}
+
+variable "kube_config_filename" {
+  default = "morsley-io-rancher-kube-config.yaml"
+}
+
+variable "instance_type" {
+  default = "t2.2xlarge" # 8 vCPUs, 32 GB RAM
+}
+
+variable "cluster_name" {
+  default = "morsley-io"
+}
+
+# ToDo --> use throughout
+variable "hostname" {
+  default = "rancher.morsley.io"
+}
+
+variable "domain_name" {
+  default = "morsley.io"
+}
