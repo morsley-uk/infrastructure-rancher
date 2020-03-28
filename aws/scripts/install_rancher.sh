5525343518e4c9171e97dfe28beec96daf3162ee
John Morsley <johnmorsley@gmail.com>
2020-03-28 13:02:51 +0000

    Our problem is rate limiting!

--- aws/scripts/install_rancher.sh
+++ aws/scripts/install_rancher.sh
@@ -15,6 +15,15 @@ chmod 400 $(pwd)/rancher/node.pem
 
 # https://rancher.com/docs/rancher/v2.x/en/installation/k8s-install/helm-rancher/
 
+# Rancher...
+
+#helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+#helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+
+#helm repo update
+
+#kubectl create namespace cattle-system
+
 # Cert-Manager...
 
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.12/deploy/manifests/00-crds.yaml
@@ -35,18 +44,26 @@ kubectl get pods --namespace cert-manager
 # Rancher...
 
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+#helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
 
 helm repo update
 
 kubectl create namespace cattle-system
 
-helm install rancher rancher-stable/rancher \
+#helm install rancher rancher-stable/rancher \
+#  --namespace cattle-system \
+#  --version v2.3.5 \
+#  --set hostname=rancher.morsley.io \
+#  --set ingress.tls.source=letsEncrypt \
+#  --set letsEncrypt.email=letsencrypt@morsley.uk \
+#  --wait
+
+helm install rancher rancher-latest/rancher \
   --namespace cattle-system \
-  --version v2.3.5 \
   --set hostname=rancher.morsley.io \
   --set ingress.tls.source=letsEncrypt \
   --set letsEncrypt.email=letsencrypt@morsley.uk \
-  --set addLocal=true \
+  --set letsEncrypt.environment=staging \
   --wait
 
 kubectl -n cattle-system rollout status deploy/rancher
